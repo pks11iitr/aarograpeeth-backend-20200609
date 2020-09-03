7cf9d3c73e72cd2e7d60c036aa0cc36f8c211804
pankaj sengar <pks11iitr@gmail.com>
2020-09-03 13:46:59 +0530

    forgot password apis

--- config/sms-templates.php
+++ config/sms-templates.php
@@ -2,4 +2,5 @@
 return [
     'register'=>'Your OTP is {{otp}} For Aarogyapeeth.com',
     'login'=>'Your OTP is {{otp}} For Aarogyapeeth.com',
+    'reset'=>'Your OTP is {{otp}} For Aarogyapeeth.com',
 ];
