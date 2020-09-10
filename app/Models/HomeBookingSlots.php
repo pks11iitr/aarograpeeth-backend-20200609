2928729186cf6c9d3f9b2024bb6247ca32337710
pankaj sengar <pks11iitr@gmail.com>
2020-09-10 19:37:29 +0530

    booking session edit in admin for therapist

--- app/Models/HomeBookingSlots.php
+++ app/Models/HomeBookingSlots.php
@@ -10,6 +10,9 @@ class HomeBookingSlots extends Model
 
     protected $fillable=['order_id', 'grade', 'date', 'time', 'display_time', 'status', 'slot_id', 'is_instant', 'assigned_therapist'];
 
+    public function assignedTo(){
+        return $this->belongsTo('App\Models\Therapist', 'assigned_therapist');
+    }
 
     public function timeslot(){
         return $this->belongsTo('App\Models\DailyBookingsSlots', 'slot_id');
