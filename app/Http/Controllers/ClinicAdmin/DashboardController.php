97cbabdb204175558512e7e2795ac14e84e155e3
pankaj sengar <pks11iitr@gmail.com>
2020-09-24 17:42:05 +0530

    dashboard work clinic admin

--- app/Http/Controllers/ClinicAdmin/DashboardController.php
+++ app/Http/Controllers/ClinicAdmin/DashboardController.php
@@ -14,14 +14,18 @@ class DashboardController extends Controller
     public function index(Request $request){
 
         $user=auth()->user();
-
+        $clinic=Clinic::where('user_id', $user->id)->first();
         $completed=BookingSlot::where('status', 'completed')
+            ->where('clinic_id', $clinic->id)
             ->count();
         $completed_amount=BookingSlot::where('status', 'completed')
+            ->where('clinic_id', $clinic->id)
             ->sum('price');
         $inprocess=BookingSlot::where('status', '!=', 'completed')
+            ->where('clinic_id', $clinic->id)
             ->count();
         $inprocess_amount=BookingSlot::where('status', '!=', 'completed')
+            ->where('clinic_id', $clinic->id)
             ->count();
 
         return view('clinicadmin.home', compact('completed', 'inprocess', 'completed_amount', 'inprocess_amount'));
