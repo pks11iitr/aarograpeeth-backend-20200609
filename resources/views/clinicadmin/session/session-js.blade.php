a35667b7c86a9613da3e9da4df7aeab9b17ddf2e
pankaj sengar <pks11iitr@gmail.com>
2020-10-26 14:27:17 +0530

    booking-edit route changed admin

--- resources/views/clinicadmin/session/session-js.blade.php
+++ resources/views/clinicadmin/session/session-js.blade.php
@@ -4,7 +4,7 @@
         $("#booking-form-section").html('')
         $.ajax({
 
-            url: '{{route('order.booking.edit')}}',
+            url: '{{route('session.booking.edit')}}',
             method: 'get',
             data:{type:type, id:id},
             success: function(data){
@@ -20,7 +20,7 @@
         $("#time-slots").html('')
         $.ajax({
 
-            url: '{{route('clinic.available.slots')}}',
+            url: '{{route('clinicadmin.available.slots')}}',
             method: 'get',
             datatype:'json',
             data:{clinic_id:$("#slot-clinic-id").val(), date:$("#slot-date").val(), grade:$("#booking-grade").val()},
@@ -46,60 +46,10 @@
         $("#therapist-list").html('')
         $.ajax({
 
-            url: '{{route('clinic.available.therapist')}}',
+            url: '{{route('clinicadmin.available.therapist')}}',
             method: 'get',
             datatype:'json',
             data:{clinic_id:$("#slot-clinic-id").val(), slot_id:$("#time-slots").val(), },
-            success: function(data){
-                html='<option value="">Select Therapist</option>'
-                for(var i = 0; i < data.length; i++) {
-
-                    html=html+'<option value="'+data[i].id+'">'+data[i].name+'</option>'
-
-                }
-
-                $("#therapist-list").html(html)
-            },
-
-        });
-    }
-
-
-    function getTimeSlotList1(){
-        $("#therapist-list").html('')
-        $("#time-slots").html('')
-        $.ajax({
-
-            url: '{{route('therapy.available.slots')}}',
-            method: 'get',
-            datatype:'json',
-            data:{therapy_id:$("#slot-therapy-id").val(), date:$("#slot-date").val(), grade:$("#booking-grade").val()},
-            success: function(data){
-                html='<option value="">Select Time</option>'
-                for(var i = 0; i < data.length; i++) {
-                    if(data[i].is_active==1){
-                        html=html+'<option value="'+data[i].id+'">'+data[i].date+' '+data[i].display+'</option>'
-                    }else{
-                        html=html+'<option value="'+data[i].id+'" disabled>'+data[i].date+' '+data[i].display+'</option>'
-                    }
-
-                }
-
-                $("#time-slots").html(html)
-            },
-
-        });
-
-    }
-
-    function getAvailableTherapist1(){
-        $("#therapist-list").html('')
-        $.ajax({
-
-            url: '{{route('therapy.available.therapist')}}',
-            method: 'get',
-            datatype:'json',
-            data:{therapy_id:$("#slot-therapy-id").val(), slot_id:$("#time-slots").val(), },
             success: function(data){
                 html='<option value="">Select Therapist</option>'
                 for(var i = 0; i < data.length; i++) {
