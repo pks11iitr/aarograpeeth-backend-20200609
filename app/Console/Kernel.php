85afe91b7fbac245284c9e3e1eee529b5b884b29
pankaj sengar <pks11iitr@gmail.com>
2020-12-11 12:33:26 +0530

    minor fixes for canceleld bookings

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('homebooking:assigntherapist')->withoutOverlapping();
+        $schedule->command('clinicbooking:assigntherapist')->withoutOverlapping();
     }
 
     /**
